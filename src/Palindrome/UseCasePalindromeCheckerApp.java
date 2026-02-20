bb8143dbbb0d14e754e84271c5fbf98067702bde
Meghana-0226 <meghanaduttuluri@gmail.com>
2026-02-20 10:03:17 +0530

    UC1

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -2,6 +2,8 @@ package Palindrome;
 
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Hello palindrome");
+        System.out.println(" Welcome To The Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized successfully...");
     }
 }
