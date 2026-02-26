718d302535ae0133427c54e853a2f010228fba85
Meghana-0226 <meghanaduttuluri@gmail.com>
2026-02-26 19:36:50 +0530

    UC7

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,21 +1,23 @@
 package Palindrome;
 
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "radar";
-        char [] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length-1;
-        boolean ispalindrome = true;
-        while (start < end)
-        {if (chars[start] != chars[end]) {
-            ispalindrome = false;
-            break;
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
         }
-            start++;
-            end--;
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
         }
-        System.out.println("INPUT TEXT: " + input);
-        System.out.println("Is it a palindrome? : " + ispalindrome);
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
