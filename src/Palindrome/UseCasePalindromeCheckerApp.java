3e7aadde1200faabbdd940a6ee13635ed37b98d8
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-27 10:25:13 +0530

    UC12-Choosing a palindrome algorithm dynamically

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,26 +1,53 @@
 package Palindrome;
 
-import java.util.Scanner;
-class PalindromeChecker {
-    public boolean CheckPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+import java.util.*;
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
         return true;
+    }
+}
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+class PalindromeChecker {
+    private PalindromeStrategy strategy;
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+    public boolean isPalindrome(String input) {
+        return strategy.check(input);
     }
 }
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.CheckPalindrome(input);
+        PalindromeStrategy strategy = new StackStrategy();
+        PalindromeChecker checker = new PalindromeChecker(strategy);
+        boolean result = checker.isPalindrome(input);
         System.out.println("isPalindrome? : " + result);
         sc.close();
     }
