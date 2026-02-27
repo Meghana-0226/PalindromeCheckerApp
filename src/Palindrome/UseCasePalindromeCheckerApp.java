656ec41f97e176fff76795332f8c750eb704ba58
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-27 10:40:14 +0530

    UC13- Comparing the performance of different palindrome approaches

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,54 +1,31 @@
 package Palindrome;
 
-import java.util.*;
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
+import java.util.Scanner;
+class PalindromeService {
     public boolean check(String input) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
                 return false;
             }
+            start++;
+            end--;
         }
         return true;
     }
 }
-class DequeStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-class PalindromeChecker {
-    private PalindromeStrategy strategy;
-    public PalindromeChecker(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-    public boolean isPalindrome(String input) {
-        return strategy.check(input);
-    }
-}
-public class UseCasePalindromeCheckerApp {
+public class UseCasePalindromeCheckerApp{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        PalindromeStrategy strategy = new StackStrategy();
-        PalindromeChecker checker = new PalindromeChecker(strategy);
-        boolean result = checker.isPalindrome(input);
-        System.out.println("isPalindrome? : " + result);
+        PalindromeService service = new PalindromeService();
+        long startTime = System.nanoTime();
+        boolean result = service.check(input);
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+        System.out.println(input + " isPalindrome? : " + result);
+        System.out.println("execution time: " + executionTime + " ns");
         sc.close();
     }
 }
