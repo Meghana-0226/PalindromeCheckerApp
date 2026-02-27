cf306add9bf81e099d6e50e80bb514d2f20f0e6c
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-27 09:31:56 +0530

    UC9-Checked palindrome using recursion

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,23 +1,20 @@
 package Palindrome;
 
-import java.util.LinkedList;
 public class UseCasePalindromeCheckerApp {
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
+    }
     public static void main(String[] args) {
-        String input = "level";
-        System.out.println("Input: " + input);
-        LinkedList<Character> list = new LinkedList<>();
-        for (char ch : input.toCharArray()) {
-            list.add(ch);
-        }
-        boolean isPalindrome = true;
-        while (list.size() > 1) {
-            char first = list.removeFirst();
-            char last = list.removeLast();
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("isPalindrome : " + isPalindrome);
+        String input = "madam";
+        System.out.println("input:" + input);
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+        System.out.println("isPalindrome : " + result);
     }
 }
