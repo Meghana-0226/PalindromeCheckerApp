8366a77e4159d400268cfa63c71134ed62db2a53
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-27 10:11:52 +0530

    UC11-Encapsulate palindrome logic in a class.  e

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,19 +1,27 @@
 package Palindrome;
 
 import java.util.Scanner;
+class PalindromeChecker {
+    public boolean CheckPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine().trim();
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-        boolean isPalindrome = true;
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("isPalindrome?  : " + isPalindrome);
+        String input = sc.nextLine();
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean result = checker.CheckPalindrome(input);
+        System.out.println("isPalindrome? : " + result);
         sc.close();
     }
 }
